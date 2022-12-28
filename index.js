2ebe0a1f322b2d1ea7505b8e7746b3159dc5f81f
Gaston <Gastonbrunijavier@gmail.com>
2022-12-28 15:31:32 -0300

    Cambios varios.
    -Agregamos dependencias EJS, EXPRESS.
    -Creamos vistas mediante ejs.
    -Creamos variables de entorno.
    -Agregamos imagenes y archivo css.

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const router = require('./routes');
+
+// importar valores de variables.env
+require('dotenv').config({path: 'variables.env'});
+
+// crear una app de express
+const app = express();
+
+// Habilitar EJS como template engine
+app.set('view engine', 'ejs');
+
+// Ubicacion vistas
+app.set('views', path.join(__dirname, './views'));
+
+// Archivos estaticos
+app.use(express.static('public'));
+
+// Routing
+app.use('/', router());
+
+// Agrega el puerto
+app.listen(process.env.PORT, () => {
+    console.log('El servidor esta funcionando');
+})
